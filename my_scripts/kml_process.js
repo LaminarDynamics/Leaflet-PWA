544da8d2c40885efddb13e232af33e294f18e88f
LaminarDynamics <LaminarDynamics@gmail.com>
2022-08-01 22:12:18 -0800

    kml_process.js is all janked up. But now it works offline

--- my_scripts/kml_process.js
+++ my_scripts/kml_process.js
@@ -2,6 +2,45 @@
 // 5-3-22
 // 5-5-22
 
+let my_data;
+
+
+function NewKmlToArray(kml) {
+    parser = new DOMParser();
+    console.log("DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoooyp")
+
+    xmlDoc = parser.parseFromString(kml, "text/xml");
+    let number_of_lines = xmlDoc.getElementsByTagName("Document")[0].getElementsByTagName("Folder").length;
+
+    if (number_of_lines > 1) {
+        for (let i = 0; i < number_of_lines; i++) { // Seperate into multi-dementional array of points defining each line 
+            let item = xmlDoc.getElementsByTagName("Document")[0].getElementsByTagName("Folder")[i];
+            let run_name = item.getElementsByTagName("name")[0].textContent
+            let placemark_data = item.getElementsByTagName("Placemark")[0];
+            let line_string = placemark_data.getElementsByTagName("LineString")[0];
+            let coords = line_string.getElementsByTagName("coordinates")[0].textContent;
+
+            coords = coords.trim();
+            let line_ends = coords.split(" ");
+            // lines_data.push([run_name, line_ends[0], line_ends[1]])
+
+            lines_data.push(SplitCoordinateData([run_name, line_ends[0], line_ends[1]]));
+        }
+    }
+
+
+    else {
+        let item = xmlDoc.getElementsByTagName("Document")[0];
+        let placemark_data = item.getElementsByTagName("Placemark")[0];
+        let line_string = placemark_data.getElementsByTagName("LineString")[0];
+        let coords = line_string.getElementsByTagName("coordinates")[0].textContent;
+        coords = coords.trim();
+        let line_ends = coords.split(" ");
+
+        lines_data.push(SplitCoordinateData(["Single Line", line_ends[0], line_ends[1]]));
+        // console.log(lines_data)
+    }
+}
 
 
 function KmlToArray(file_paths) {
@@ -13,56 +52,77 @@ function KmlToArray(file_paths) {
 
 
 
-        readTextFile(kml_file)
+        // readTextFile(kml_file)
+        demo_data = ReadKmlText(kml_file);
 
         parser = new DOMParser();
-        xmlDoc = parser.parseFromString(demo_data, "text/xml");
-        let number_of_lines = xmlDoc.getElementsByTagName("Document")[0].getElementsByTagName("Folder").length;
+        console.log(my_data)
 
-        if (number_of_lines > 1) {
-            for (let i = 0; i < number_of_lines; i++) { // Seperate into multi-dementional array of points defining each line 
-                let item = xmlDoc.getElementsByTagName("Document")[0].getElementsByTagName("Folder")[i];
-                let run_name = item.getElementsByTagName("name")[0].textContent
-                let placemark_data = item.getElementsByTagName("Placemark")[0];
-                let line_string = placemark_data.getElementsByTagName("LineString")[0];
-                let coords = line_string.getElementsByTagName("coordinates")[0].textContent;
+        // xmlDoc = parser.parseFromString(my_data, "text/xml");
+        // let number_of_lines = xmlDoc.getElementsByTagName("Document")[0].getElementsByTagName("Folder").length;
 
-                coords = coords.trim();
-                let line_ends = coords.split(" ");
-                // lines_data.push([run_name, line_ends[0], line_ends[1]])
+        // if (number_of_lines > 1) {
+        //     for (let i = 0; i < number_of_lines; i++) { // Seperate into multi-dementional array of points defining each line 
+        //         let item = xmlDoc.getElementsByTagName("Document")[0].getElementsByTagName("Folder")[i];
+        //         let run_name = item.getElementsByTagName("name")[0].textContent
+        //         let placemark_data = item.getElementsByTagName("Placemark")[0];
+        //         let line_string = placemark_data.getElementsByTagName("LineString")[0];
+        //         let coords = line_string.getElementsByTagName("coordinates")[0].textContent;
 
-                lines_data.push(SplitCoordinateData([run_name, line_ends[0], line_ends[1]]));
-            }
-        }
+        //         coords = coords.trim();
+        //         let line_ends = coords.split(" ");
+        //         // lines_data.push([run_name, line_ends[0], line_ends[1]])
 
-
-        else {
-            let item = xmlDoc.getElementsByTagName("Document")[0];
-            let placemark_data = item.getElementsByTagName("Placemark")[0];
-            let line_string = placemark_data.getElementsByTagName("LineString")[0];
-            let coords = line_string.getElementsByTagName("coordinates")[0].textContent;
-            coords = coords.trim();
-            let line_ends = coords.split(" ");
-
-            lines_data.push(SplitCoordinateData(["Single Line", line_ends[0], line_ends[1]]));
-            // console.log(lines_data)
-        }
+        //         lines_data.push(SplitCoordinateData([run_name, line_ends[0], line_ends[1]]));
+        //     }
+        // }
 
 
-        function readTextFile(file) {
-            var rawFile = new XMLHttpRequest();
-            rawFile.open("GET", file, false);
-            rawFile.onreadystatechange = function () {
-                if (rawFile.readyState === 4) {
-                    if (rawFile.status === 200 || rawFile.status == 0) {
-                        var allText = rawFile.responseText;
-                        // alert(allText);
-                        demo_data = allText;
-                    }
+        // else {
+        //     let item = xmlDoc.getElementsByTagName("Document")[0];
+        //     let placemark_data = item.getElementsByTagName("Placemark")[0];
+        //     let line_string = placemark_data.getElementsByTagName("LineString")[0];
+        //     let coords = line_string.getElementsByTagName("coordinates")[0].textContent;
+        //     coords = coords.trim();
+        //     let line_ends = coords.split(" ");
+
+        //     lines_data.push(SplitCoordinateData(["Single Line", line_ends[0], line_ends[1]]));
+        //     // console.log(lines_data)
+        // }
+
+        function ReadKmlText(file) {
+            let file_text = "";
+            fetch(file)
+                .then(response => response.text())
+                .then(text => Do(text))
+            // outputs the content of the text file
+
+            function Do(my_text) {
+                my_data = my_text;
+                if (my_data != "") {
+                    console.log(my_data)
+                    NewKmlToArray(my_data);
                 }
+                // file_text = my_text;
             }
-            rawFile.send(null);
+            // return file_text;
         }
+
+
+        // function readTextFile(file) {
+        //     var rawFile = new XMLHttpRequest();
+        //     rawFile.open("GET", file, false);
+        //     rawFile.onreadystatechange = function () {
+        //         if (rawFile.readyState === 4) {
+        //             if (rawFile.status === 200 || rawFile.status == 0) {
+        //                 var allText = rawFile.responseText;
+        //                 // alert(allText);
+        //                 demo_data = allText;
+        //             }
+        //         }
+        //     }
+        //     rawFile.send(null);
+        // }
 
     });
 
