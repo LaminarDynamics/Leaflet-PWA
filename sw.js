91247bf7fba8ea2e3dc99c51bafb27f771e04f82
LaminarDynamics <LaminarDynamics@gmail.com>
2022-08-02 16:00:20 -0800

    Turned PWA back on

--- sw.js
+++ sw.js
@@ -30,32 +30,32 @@ let my_assets = [
 ];
 
 
-// self.addEventListener('install', (e) => {
-//     console.log('[Service Worker] Install');
-//     e.waitUntil((async () => {
-//         const cache = await caches.open(cache_name);
-//         console.log('[Service Worker] Caching all: app shell and content');
-//         await cache.addAll(my_assets);
-//     })());
-// });
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(cache_name);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(my_assets);
+    })());
+});
 
 
-// //  Fetch request
-// self.addEventListener('fetch', (e) => { // Fires when resourse is grabbed from local. If not in local adds needed file to local after download
-//     e.respondWith((async () => {
-//         const r = await caches.match(e.request);
-//         console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-//         if (r) { return r; }
-//         const response = await fetch(e.request);
-//         const cache = await caches.open(cache_name);
-//         console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-//         cache.put(e.request, response.clone());
-//         return response;
-//     })());
-// });
+//  Fetch request
+self.addEventListener('fetch', (e) => { // Fires when resourse is grabbed from local. If not in local adds needed file to local after download
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(e.request);
+        const cache = await caches.open(cache_name);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
 
 // Clear cache
-caches.delete(cache_name);
+// caches.delete(cache_name);
 // self.addEventListener('activate', (e) => {
 //     e.waitUntil(caches.keys().then((keyList) => {
 //         return Promise.all(keyList.map((key) => {
